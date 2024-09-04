d1406b526d6c41da3a30e00f47295acbbd131630
DNAnh01 <donguyenanhgithub@gmail.com>
2024-09-05 02:01:30 +0700

    update 050924

---
+++ vscode-script.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const checkElement = setInterval(() => {
+        const commandDialog = document.querySelector(".quick-input-widget");
+        if (commandDialog) {
+            // Apply the blur effect immediately if the command dialog is visible
+            if (commandDialog.style.display !== "none") {
+                runMyScript();
+            }
+            // Create a DOM observer to listen for changes in element's attributes
+            const observer = new MutationObserver((mutations) => {
+                mutations.forEach((mutation) => {
+                    if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                        if (commandDialog.style.display === 'none') {
+                            handleEscape();
+                        } else {
+                            runMyScript();
+                        }
+                    }
+                });
+            });
+
+            observer.observe(commandDialog, { attributes: true });
+
+            // Clear the interval once the observer is set
+            clearInterval(checkElement);
+        } else {
+            console.log("Command dialog not found yet. Retrying...");
+        }
+    }, 500); // Check every 500ms
+
+    document.addEventListener('keydown', function (event) {
+        if ((event.metaKey || event.ctrlKey) && event.key === 'p') {
+            event.preventDefault();
+            runMyScript();
+        } else if (event.key === 'Escape' || event.key === 'Esc') {
+            event.preventDefault();
+            handleEscape();
+        }
+    });
+
+    function runMyScript() {
+        const targetDiv = document.querySelector(".monaco-workbench");
+
+        // Remove existing element if it already exists
+        const existingElement = document.getElementById("command-blur");
+        if (existingElement) {
+            existingElement.remove();
+        }
+
+        // Create and configure the new element
+        const newElement = document.createElement("div");
+        newElement.setAttribute('id', 'command-blur');
+
+        newElement.addEventListener('click', function () {
+            newElement.remove();
+        });
+
+        // Append the new element as a child of the targetDiv
+        targetDiv.appendChild(newElement);
+    }
+
+    function handleEscape() {
+        const element = document.getElementById("command-blur");
+        if (element) {
+            element.click();
+        }
+    }
+});
